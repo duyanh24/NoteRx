cb43e47ce70db2e9786417132dbdd0e389896f40
AnhLD <anhld@Nguyens-Mac-mini-2.local>
2020-09-17 13:33:34 +0700

    first commit

---
+++ NoteRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteRx
+//
+//  Created by AnhLD on 9/17/20.
+//  Copyright © 2020 AnhLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
